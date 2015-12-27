3e9de86818f390839f99c65d2ad50098c196f87a
igor <cuckovic@gmail.com>
2015-12-27 20:50:40 +0100

    webSQL

--- js/script.js
+++ js/script.js
@@ -5,104 +5,50 @@
 //
 document.addEventListener("deviceready", onDeviceReady, false);
 
-var currentRow;
-// Populate the database
-//
-function populateDB(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id INTEGER PRIMARY KEY AUTOINCREMENT, name, number)');
-}
+// http://www.oodlestechnologies.com/blogs/Use-of-Web-SQL-in-Phonegap
+// https://github.com/orbitaloop/WebSqlSync
 
-// Query the database
-//
-function queryDB(tx) {
-    tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
-}
+function onDeviceReady () {
 
-function searchQueryDB(tx) {
-    tx.executeSql("SELECT * FROM DEMO where name like ('%"+ document.getElementById("txtName").value + "%')",
-            [], querySuccess, errorCB);
-}
-// Query the success callback
-//
-function querySuccess(tx, results) {
-    var tblText='<table id="t01"><tr><th>ID</th> <th>Name</th> <th>Number</th></tr>';
-    var len = results.rows.length;
-    for (var i = 0; i < len; i++) {
-        var tmpArgs=results.rows.item(i).id + ",'" + results.rows.item(i).name
-                + "','" + results.rows.item(i).number+"'";
-        tblText +='<tr onclick="goPopup('+ tmpArgs + ');"><td>' + results.rows.item(i).id +'</td><td>'
-                + results.rows.item(i).name +'</td><td>' + results.rows.item(i).number +'</td></tr>';
-    }
-    tblText +="</table>";
-    document.getElementById("tblDiv").innerHTML = tblText;
-}
+    var app = angular.module("toDoApp", []);
 
-//Delete query
-function deleteRow(tx) {
-  tx.executeSql('DELETE FROM DEMO WHERE id = ' + currentRow, [], queryDB, errorCB);
-}
+    app.controller("TodoCtrl", ["$scope", function ($scope) {
 
-// Transaction error callback
-//
-function errorCB(err) {
-    alert("Error processing SQL: "+err.code);
-}
+        $scope.todos = [];
 
-// Transaction success callback
-//
-function successCB() {
-    var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-    db.transaction(queryDB, errorCB);
-}
+        var Todo = $data.define("Todo", {
+            task: String,
+            place: String,
+            done: Boolean
+        });
 
- // Cordova is ready
-//
-function onDeviceReady() {
-    var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-    db.transaction(populateDB, errorCB, successCB);
-}
+        Todo.readAll().then(function (todos) {
+            $scope.$apply(function () {
+                $scope.todos = todos;
+            });
+        });
 
-//Insert query
-//
-function insertDB(tx) {
-    tx.executeSql('INSERT INTO DEMO (name,number) VALUES ("' +document.getElementById("txtName").value
-            +'","'+document.getElementById("txtNumber").value+'")');
-}
+        $scope.addNew = function (todo) {
+            Todo.save(todo).then(function (todo) {
+                $scope.$apply(function () {
+                    $scope.newTodo = null;
+                    $scope.todos.push(todo);
+                });
+            });
+        };
 
-function goInsert() {
-    var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-    db.transaction(insertDB, errorCB, successCB);
-}
+        $scope.remove = function (todo) {
 
-function goSearch() {
-    var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-    db.transaction(searchQueryDB, errorCB);
-}
+            todo.remove();
 
-function goDelete() {
-     var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-     db.transaction(deleteRow, errorCB);
-     document.getElementById('qrpopup').style.display='none';
-}
+            Todo.readAll().then(function (todos) {
+                $scope.$apply(function () {
+                    $scope.todos = todos;
+                });
+            });         
+        };
 
-//Show the popup after tapping a row in table
-//
-function goPopup(row,rowname,rownum) {
-    currentRow = row;
+    }]);
+    
+};
 
-    d3.select("#editId").text(row);
-    document.getElementById("qrpopup").style.display="block";
-    document.getElementById("editNameBox").value = rowname;
-    document.getElementById("editNumberBox").value = rownum;
-}
-
-function editRow(tx) {
-    tx.executeSql('UPDATE DEMO SET name ="'+document.getElementById("editNameBox").value+
-            '", number= "'+document.getElementById("editNumberBox").value+ '" WHERE id = '
-            + currentRow, [], queryDB, errorCB);
-}
-function goEdit() {
-    var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-    db.transaction(editRow, errorCB);
-    document.getElementById('qrpopup').style.display='none';
-}
